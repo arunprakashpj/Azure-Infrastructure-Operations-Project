6463529a9aa1e712c9846300e5b3960606c18dd8
Arun Prakash <arunprakashpj@gmail.com>
2021-01-16 16:46:45 +0100

    Update az policy definition

--- create_az_policy_definition.sh
+++ create_az_policy_definition.sh
@@ -2,6 +2,6 @@
 
 az policy definition create --name tagging-policy \
 							--mode indexed \
-                            --rules azure-policy.json --params tagging-policy-param.json \
+                            --rules azure-policy.json --params tagging-policy.json \
                             --mode Indexed \
                             --description "This policy deny if indexed resources in the subscription have no tags"
